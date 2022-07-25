a505c3452b1c111469b171cecc6fa651a9c8cbf5
roccovp3 <rocco.perciavalleiii@gmail.com>
2022-07-25 00:20:52 -0500

    display now shows each players hand on webpage

--- display.js
+++ display.js
@@ -1,7 +1,10 @@
 import { numPlayers } from "./blackjack.js";
+import { hands } from "./cards.js";
 export function updateDisplayCards(){
+    const handDivDealer = document.getElementById("dealer-hand");
+    handDivDealer.innerHTML = String(hands[0].cardsInHand);
     for(let i = 1; i < numPlayers+1; i++){
         const handDiv = document.getElementById("player-hand"+String(i));
-        handDiv.innerHTML = "test";
+        handDiv.innerHTML = String(hands[i].cardsInHand);
     }
 }
